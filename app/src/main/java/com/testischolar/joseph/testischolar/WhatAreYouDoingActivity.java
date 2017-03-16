1bb872747be78f41cde42f70e57050a2d9c286e3
jmessou <mejc2014@vt.edu>
2017-03-16 07:20:06 -0500

    App_change1

---
+++ app/src/main/java/com/testischolar/joseph/testischolar/WhatAreYouDoingActivity.java
@@ -0,0 +1,13 @@
+package com.testischolar.joseph.testischolar;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class WhatAreYouDoingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_what_are_you_doing);
+    }
+}
